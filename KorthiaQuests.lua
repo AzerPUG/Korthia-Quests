75219f3db7c4e9ceacf52467e3bee446aeab8b61
Arco Dielhof <arcodielhof@hotmail.com>
2021-07-01 11:10:34 +0200

    First Version, shows quests and IDs.

---
+++ KorthiaQuests.lua
@@ -0,0 +1,83 @@
+if AZP == nil then AZP = {} end
+if AZP.VersionControl == nil then AZP.VersionControl = {} end
+
+AZP.VersionControl["KorthiaQuests"] = 1
+if AZP.KorthiaQuests == nil then AZP.KorthiaQuests = {} end
+if AZP.KorthiaQuests.Events == nil then AZP.KorthiaQuests.Events = {} end
+
+local EventFrame, AZPKQSelfFrame = nil, nil
+
+function AZP.KorthiaQuests:OnLoadSelf()
+    EventFrame = CreateFrame("Frame", nil)
+    EventFrame:RegisterEvent("VARIABLES_LOADED")
+    EventFrame:SetScript("OnEvent", function(...) AZP.KorthiaQuests:OnEvent(...) end)
+
+    AZPKQSelfFrame = CreateFrame("Frame", nil, UIParent, "BackdropTemplate")
+    AZPKQSelfFrame:SetPoint("CENTER", 0, 250)
+    AZPKQSelfFrame:SetSize(275, 380)
+    AZPKQSelfFrame:SetBackdrop({
+        bgFile = "Interface/Tooltips/UI-Tooltip-Background",
+        edgeFile = "Interface/Tooltips/UI-Tooltip-Border",
+        edgeSize = 12,
+        insets = { left = 1, right = 1, top = 1, bottom = 1 },
+    })
+    AZPKQSelfFrame:SetBackdropColor(0.25, 0.25, 0.25, 0.80)
+    AZPKQSelfFrame.header = AZPKQSelfFrame:CreateFontString("AZPKQSelfFrame", "ARTWORK", "GameFontNormalHuge")
+    AZPKQSelfFrame.header:SetPoint("TOP", 0, -10)
+    AZPKQSelfFrame.header:SetText("|cFF00FFFFAzerPUG's Korthia Quests|r")
+
+    AZPKQSelfFrame.closeButton = CreateFrame("Button", nil, AZPKQSelfFrame, "UIPanelCloseButton")
+    AZPKQSelfFrame.closeButton:SetSize(25, 25)
+    AZPKQSelfFrame.closeButton:SetPoint("TOPRIGHT", AZPKQSelfFrame, "TOPRIGHT", 2, 2)
+    AZPKQSelfFrame.closeButton:SetScript("OnClick", function() AZP.KorthiaQuests:ShowHideFrame() end )
+end
+
+function AZP.KorthiaQuests:FillUserFrame()
+    local Quests = AZP.KorthiaQuests.Quests
+    if AZPKQSelfFrame.QuestLabels == nil then AZPKQSelfFrame.QuestLabels = {} end
+    for i = 1, #Quests.IDs do
+        AZPKQSelfFrame.QuestLabels[i] = AZPKQSelfFrame:CreateFontString("AZPKQSelfFrame", "ARTWORK", "GameFontNormal")
+        AZPKQSelfFrame.QuestLabels[i]:SetPoint("TOPLEFT", 10, -20 * i -20)
+        local curID = Quests.IDs[i]
+        local QColor = ""
+        if C_QuestLog.IsQuestFlaggedCompleted(curID) == true then
+            QColor = "|cFF00FF00"
+        elseif C_QuestLog.IsOnQuest(curID) == true then
+            QColor = "|cFFFFFF00"
+        elseif C_QuestLog.IsQuestFlaggedCompleted(curID) == false then
+            QColor = "|cFFFF0000"
+        end
+        AZPKQSelfFrame.QuestLabels[i]:SetText(QColor .. curID .. " - " .. Quests[curID].Name .. "|r")
+    end
+end
+
+function AZP.KorthiaQuests:OnEvent(self, event, ...)
+    if event == "VARIABLES_LOADED" then
+        AZP.KorthiaQuests.Events:VariablesLoaded()
+    end
+end
+
+function AZP.KorthiaQuests.Events:VariablesLoaded()
+    AZP.KorthiaQuests:FillUserFrame()
+end
+
+function AZP.KorthiaQuests:ShowHideFrame()
+    if AZPKQSelfFrame:IsShown() then
+        AZPKQSelfFrame:Hide()
+        --AZPCoreShown = false
+    elseif not AZPKQSelfFrame:IsShown() then
+        AZPKQSelfFrame:Show()
+        --AZPCoreShown = true
+    end
+end
+
+AZP.KorthiaQuests:OnLoadSelf()
+
+AZP.SlashCommands["KQ"] = function()
+    AZP.KorthiaQuests:ShowHideFrame()
+end
+
+AZP.SlashCommands["kq"] = AZP.SlashCommands["KQ"]
+AZP.SlashCommands["korthia"] = AZP.SlashCommands["KQ"]
+AZP.SlashCommands["korthia quests"] = AZP.SlashCommands["KQ"]
+AZP.SlashCommands["Korthia Quests"] = AZP.SlashCommands["KQ"]
---
+++ dataTable.lua
@@ -0,0 +1,24 @@
+if AZP == nil then AZP = {} end
+if AZP.KorthiaQuests == nil then AZP.KorthiaQuests = {} end
+
+AZP.KorthiaQuests.Quests =
+{
+    IDs = {63860, 63892, 63899, 63909, 63910, 63911, 63912, 63913, 63914, 63915, 63916, 63917, 63918, 63919, 63920, 63921, 63924},
+    [63860] = {Name = "Talisman of the Eternal Scholar",},
+    [63892] = {Name = "Diviner's Rune Chits",},
+    [63899] = {Name = "Book of Binding: The Mad Witch",},
+    [63909] = {Name = "Guise of the Changeling",},
+    [63910] = {Name = "The Netherstar",},
+    [63911] = {Name = "Singing Steel Ingot",},
+    [63912] = {Name = "Celestial Shadowlands Chart",},
+    [63913] = {Name = "Unstable Explosive Orb",},
+    [63914] = {Name = "Cipher of Understanding",},
+    [63915] = {Name = "Drum of the Death Loa",},
+    [63916] = {Name = "Sack of Strange Soil",},
+    [63917] = {Name = "Everliving Statuette",},
+    [63918] = {Name = "Obelisk of Dark Tidings",},
+    [63919] = {Name = "Book of Binding: The Tormented Sorcerer",},
+    [63920] = {Name = "Enigmatic Decrypting Device",},
+    [63921] = {Name = "Ring of Self Reflection",},
+    [63924] = {Name = "Gorak Claw Fetish",},
+}
